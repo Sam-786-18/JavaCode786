6b2c87065da2ed6bceaf1b0306f5375c4aedcac0
Sam-786-18 <smscjp28@gmail.com>
2026-07-22 19:15:32 +0530

    add

---
+++ src/com/st/java/code/ASCII_Convertions.java
@@ -0,0 +1,13 @@
+package com.st.java.code;
+
+public class ASCII_Convertions {
+	public static void main(String[] args) {
+		char c='a';
+		System.out.println((byte)c);
+		System.out.println((byte)'d');
+		System.out.println((char)100);
+	}
+	
+
+
+}
